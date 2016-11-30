8ddfcabe35c2336b2430b3088c32952f36bb72b0
Diego Leong <stefanogonzales@gmail.com>
2016-11-29 17:34:41 -0800

    ajo;

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 $(function() {
     $('.button.desktop').on('click', function(event) {
-        $('.desktop.os').show();
+        $('.os.active').removeClass("active");
+        $('.desktop.os').addClass('active');
+        $('.button.active').removeClass('active');
+        $(this).addClass('active');
+    })
+    $('.button.mobile').on('click', function(event) {
+        $('.os.active').removeClass("active");
+        $('.mobile.os').addClass('active');
+        $('.button.active').removeClass('active');
+        $(this).addClass('active');
+        
     })
 })
